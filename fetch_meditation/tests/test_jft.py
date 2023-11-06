f3dd0dd73a484d59892ebf870a403d4a34b8cc07
Patrick Joyce <pjaudiomv@gmail.com>
2023-11-06 12:14:04 -0500

    use fixture

--- fetch_meditation/tests/test_jft.py
+++ fetch_meditation/tests/test_jft.py
@@ -1,33 +1,33 @@
 import pytest
-
+from fetch_meditation.jft_language import JftLanguage
+from fetch_meditation.jft_settings import JftSettings
 from fetch_meditation.english_jft import EnglishJft
 from fetch_meditation.german_jft import GermanJft
+from fetch_meditation.portuguese_jft import PortugueseJft
+from fetch_meditation.japanese_jft import JapaneseJft
+from fetch_meditation.russian_jft import RussianJft
 from fetch_meditation.jft import Jft
-from fetch_meditation.jft_settings import JftSettings
-from fetch_meditation.jft_language import JftLanguage
 
+@pytest.fixture(params=[
+    (EnglishJft, JftLanguage.English),
+    (GermanJft, JftLanguage.German),
+    (PortugueseJft, JftLanguage.Portuguese),
+    (JapaneseJft, JftLanguage.Japanese),
+    (RussianJft, JftLanguage.Russian),
+])
+def language_cls(request):
+    return request.param
 
-@pytest.fixture
-def english_jft_settings():
-    return JftSettings(JftLanguage.English)
+def test_jft_language_property(language_cls):
+    jft_cls, language = language_cls
+    jft_settings = JftSettings(language)
+    jft_instance = Jft(jft_settings)
 
+    assert jft_instance.language == language
 
-@pytest.fixture
-def german_jft_settings():
-    return JftSettings(JftLanguage.German)
+def test_get_instance(language_cls):
+    jft_cls, jft_lang = language_cls
+    jft_settings = JftSettings(jft_lang)
+    jft_instance = Jft.get_instance(jft_settings)
 
-
-def test_jft_language_property(english_jft_settings, german_jft_settings):
-    english_jft = Jft(english_jft_settings)
-    german_jft = Jft(german_jft_settings)
-
-    assert english_jft.language == JftLanguage.English
-    assert german_jft.language == JftLanguage.German
-
-
-def test_get_instance(english_jft_settings, german_jft_settings):
-    english_jft = Jft.get_instance(english_jft_settings)
-    german_jft = Jft.get_instance(german_jft_settings)
-
-    assert isinstance(english_jft, EnglishJft)
-    assert isinstance(german_jft, GermanJft)
+    assert isinstance(jft_instance, jft_cls)
