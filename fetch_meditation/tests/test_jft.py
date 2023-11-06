fd646dfa47b0a3a801a06953524ca13759f3fce0
Patrick Joyce <pjaudiomv@gmail.com>
2023-11-06 12:24:05 -0500

    fix fmt

--- fetch_meditation/tests/test_jft.py
+++ fetch_meditation/tests/test_jft.py
@@ -8,6 +8,7 @@ from fetch_meditation.japanese_jft import JapaneseJft
 from fetch_meditation.russian_jft import RussianJft
 from fetch_meditation.jft import Jft
 
+
 @pytest.fixture(params=[
     (EnglishJft, JftLanguage.English),
     (GermanJft, JftLanguage.German),
@@ -18,12 +19,14 @@ from fetch_meditation.jft import Jft
 def language_cls(request):
     return request.param
 
+
 def test_jft_language_property(language_cls):
     jft_cls, language = language_cls
     jft_settings = JftSettings(language)
     jft_instance = Jft(jft_settings)
 
     assert jft_instance.language == language
+
 
 def test_get_instance(language_cls):
     jft_cls, jft_lang = language_cls
